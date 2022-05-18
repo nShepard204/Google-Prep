572ca0581c1a25165f2750ab5a699572fb274a2f
ShepDogFGC <71411118+ShepDogFGC@users.noreply.github.com>
2022-05-18 14:06:43 -0400

    First push
    We gettin there

---
+++ src/main/java/com/example/Examples.java
@@ -0,0 +1,17 @@
+package com.example;
+
+/**
+ * Hello world!
+ */
+public final class Examples {
+    private Examples() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
